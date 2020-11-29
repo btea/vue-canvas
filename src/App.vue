d0e515ee6efad54f0df4d562d510d2a6bbb807c9
btea <2356281422@qq.com>
2020-11-29 17:07:33 +0800

    :tada: init :tada:

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>hello world</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+<style lang="less" scoped>
+  h1{
+    border-radius: 3px;
+  }
+</style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    // css: {
+    //     loaderOptions: {
+    //         postcss: {
+    //             plugins: ['postcss-preset-env']
+    //         }
+    //     }
+    // }
+}
